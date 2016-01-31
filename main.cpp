faa841cc32ab4d1f8ca8712a6a393711312827dd
vincuk <vincUk@VinAir.local>
2016-01-31 13:31:43 +0300

    rollback

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <sys/msg.h>
-#include <unistd.h>
+#include <stdio.h>
 
 int main() {
+	FILE *outfile;
 	int result;
 	int msqid;
-	long msgtyp = 1;
 	
 	struct message {
 		long mtype;
@@ -12,14 +12,13 @@ int main() {
 	} msg;
 	
 	key_t key = ftok("/tmp/msg.temp", 0);
-	msqid = msgget(key, 0666 | IPC_CREAT);
-	result = msgrcv(msqid, (void *) &msg, sizeof(msg.mtext), msgtyp, 0);
 
-	int f = open("/home/box/message.txt", O_RDWR | O_CREAT, 0666);
-	write(f, msg.mtext, sizeof(msg.mtext));
-	close(f);
-	
+	long msgtyp = 0;
+	result = msgrcv(msqid, (void *) &msg, sizeof(msg.text),
+		msgtyp, MSG_NOERROR | IPC_NOWAIT);
+		
+	outfile = fopen("/home/box/message.txt", "w");
+	fprintf(outfile, "%s\n", msg.text);
+	fclose(outfile);
 	return 0;
 }
-
-
