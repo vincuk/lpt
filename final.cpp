7b515431298ca6d0a7898f59a03fd20b160207f4
vincuk <vincUk@VinAir.local>
2016-02-22 14:45:43 +0300

    final

--- final.cpp
+++ final.cpp
@@ -14,8 +14,8 @@
 #include <cstring>
 
 char *dirval = (char *)"/";
-// const char *filename = (char *)"/tmp/log.txt";
-// FILE *flog;
+const char *filename = (char *)"log.txt";
+FILE *flog;
 
 void not_found(int client) {
 	char buf[1024];
@@ -35,14 +35,16 @@ void not_found(int client) {
 
 void send_file(int client, FILE *f) {
 	char buf[1024];
-
+	flog = fopen(filename, "a");
 	write(client, "HTTP/1.0 200 OK\r\n", 17);
 	write(client, "Content-Type: text/html\r\n\r\n", 27);
 	fgets(buf, sizeof(buf), f);
 	while (!feof(f)) {
+		fprintf(flog, "%d\n", strlen(buf));
 		write(client, buf, strlen(buf));
 		fgets(buf, sizeof(buf), f);
 	}
+	fclose(flog);
 }
 
 void accept_request(void *arg) {
@@ -56,9 +58,9 @@ void accept_request(void *arg) {
 
 	recv(*client, buffer, bufsize, 0);
 	
-// 	flog = fopen(filename, "a");
-// 	fprintf(flog, "%s\n", buffer);
-// 	fclose(flog);
+	flog = fopen(filename, "a");
+	fprintf(flog, "%s\n", buffer);
+	fclose(flog);
 	i = 0;
 	j = 0;
 	int url_s = strlen(dirval);
@@ -90,17 +92,17 @@ void accept_request(void *arg) {
 		j++;
 		k++;
 	}
-	// if (k == 1) {
-// 		char * index = (char *)"index.html";
-// 		for (j = 0; j < 10; j++) {
-// 			URL[i] = index[j];
-// 			i++;
-// 		}
-// 	}
+	if (k == 1) {
+		char * index = (char *)"index.html";
+		for (j = 0; j < 10; j++) {
+			URL[i] = index[j];
+			i++;
+		}
+	}
 	URL[i] = '\0';
-// 	flog = fopen(filename, "a");
-// 	fprintf(flog, "Requested URL: %s\n", URL);
-// 	fclose(flog);
+	flog = fopen(filename, "a");
+	fprintf(flog, "Requested URL: %s\n", URL);
+	fclose(flog);
 	if (strcasecmp(method, "GET") == 0) {
 		FILE *f = NULL;
 		f = fopen(URL, "r");
@@ -142,9 +144,9 @@ int main(int argc, char **argv) {
 				abort ();
 			}
 	}
-// 	flog = fopen(filename, "a");
-// 	fprintf (flog, "host = %s, port = %d, dir = %s\n", hostval, portval, dirval);
-// 	fclose(flog);
+	flog = fopen(filename, "a");
+	fprintf (flog, "host = %s, port = %d, dir = %s\n", hostval, portval, dirval);
+	fclose(flog);
 
 	pid_t curPID = fork();
  	if (curPID != 0) {
@@ -156,9 +158,9 @@ int main(int argc, char **argv) {
 	struct sockaddr_in address;
 	pthread_t newthread;
 
-// 	flog = fopen(filename, "a");
+	flog = fopen(filename, "a");
 	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0) {
-// 		fprintf(flog, "The socket was created\n");
+		fprintf(flog, "The socket was created\n");
 	}
 	
 	address.sin_family = AF_INET;
@@ -166,9 +168,9 @@ int main(int argc, char **argv) {
 	address.sin_port = htons(portval);
 	
 	if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0) {
-// 		fprintf(flog, "Binding Socket\n");
+		fprintf(flog, "Binding Socket\n");
 	}
-// 	fclose(flog);
+	fclose(flog);
 
 	
 	while (1) {
@@ -183,9 +185,9 @@ int main(int argc, char **argv) {
 		}
 	
 		if (new_socket > 0) {
-			// flog = fopen(filename, "a");
-// 			fprintf(flog, "The Client is connected... PID = %d\n", new_socket);
-// 			fclose(flog);
+			flog = fopen(filename, "a");
+			fprintf(flog, "The Client is connected... PID = %d\n", new_socket);
+			fclose(flog);
 		}
 
 		if (pthread_create(&newthread, NULL, (void *(*)(void *))accept_request, (void*)&new_socket) != 0) {
